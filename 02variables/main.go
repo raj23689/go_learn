35d1675634fa3c4348f29aca0fafe61caa9c14ff
Raj Majumdar <92665411+raj23689@users.noreply.github.com>
2023-10-22 23:10:53 +0530

    updated variables main.go -> bool

--- 02variables/main.go
+++ 02variables/main.go
@@ -6,4 +6,8 @@ func main() {
 	var userName string = "Raj"
 	fmt.Println(userName)
 	fmt.Printf("Variable is of type: %T \n", userName)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
 }
